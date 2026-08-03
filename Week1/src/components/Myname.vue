ad4ebd708254fdd6730b456b10a4514b2a8e4868
Yunpeng Ke <ykee0023@student.monash.edu>
2026-08-03 12:28:31 +1000

    Initial commit: FIT5032 Vue.js project (week1)

---
+++ Week1/src/components/Myname.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-name">
+    <h1>Name:<tr>Yunpeng Ke</tr> </h1>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.my-name {
+  margin-top: 20px;
+  color: #42b883;
+}
+</style>
