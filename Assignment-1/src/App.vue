0a9273b34a1ff03da42972c39a0c2ceb7bd43e19
Yunpeng Ke <ykee0023@student.monash.edu>
2026-08-25 11:38:00 +1000

    [A1.2] feat: set up app structure with Router, Bootstrap, and custom theme
    - Configure main.js with Bootstrap CSS/JS and Pinia
    - Create App.vue shell with NavBar, RouterView, FooterBar
    - Add 6 page routes with lazy loading
    - Create GreenRoots brand theme with responsive media queries

---
+++ Assignment-1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/layout/NavBar.vue'
+import FooterBar from './components/layout/FooterBar.vue'
+</script>
+
+<template>
+  <div id="app">
+    <NavBar />
+    <main>
+      <RouterView />
+    </main>
+    <FooterBar />
+  </div>
+</template>
---
+++ Assignment-1/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: () => import('../views/EventsView.vue'),
+    },
+    {
+      path: '/resources',
+      name: 'resources',
+      component: () => import('../views/ResourcesView.vue'),
+    },
+    {
+      path: '/get-involved',
+      name: 'get-involved',
+      component: () => import('../views/GetInvolvedView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
+    },
+  ],
+})
+
+export default router
