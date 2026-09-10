86194fabbac459163132ade68ba5daae750722dd
Yunpeng Ke <ykee0023@student.monash.edu>
2026-09-10 21:04:00 +1000

    [A1.3] feat: add login route to Vue Router configuration

--- Assignment-1/src/router/index.js
+++ Assignment-1/src/router/index.js
@@ -38,6 +38,11 @@ const router = createRouter({
       component: () => import('../views/ContactView.vue'),
     },
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: () => import('../views/NotFoundView.vue'),
