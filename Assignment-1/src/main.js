9626077ff4afe4a499711bc6de01926af9e64f10
Yunpeng Ke <ykee0023@student.monash.edu>
2026-09-10 20:23:00 +1000

    [A1.3] feat: initialize auth store on app startup with default admin seeding

--- Assignment-1/src/main.js
+++ Assignment-1/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/authStore'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -9,7 +10,16 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import './assets/styles/main.css'
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+async function bootstrap() {
+  const app = createApp(App)
+  const pinia = createPinia()
+  app.use(pinia)
+  app.use(router)
+
+  const authStore = useAuthStore()
+  await authStore.initAuth()
+
+  app.mount('#app')
+}
+
+bootstrap()
