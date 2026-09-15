bd543efaa2759346b46d82fc5de078eb03c5af61
Yunpeng Ke <ykee0023@student.monash.edu>
2026-09-15 19:19:41 +1000

    Update Home nav label from Week 5 to Week 7

--- week7_and_week8/src/components/BHeader.vue
+++ week7_and_week8/src/components/BHeader.vue
@@ -6,7 +6,7 @@
       <ul class="nav nav-pills">
         <li class="nav-item">
           <router-link to="/" class="nav-link" active-class="active" aria-current="page"
-            >Home (Week 5)</router-link
+            >Home (Week 7)</router-link
           >
         </li>
         <li class="nav-item">
